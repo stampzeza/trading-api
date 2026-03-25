bb0c544709070003f1c8842c8cc964f6ddcbccf3
stampzeza <tamanucha@gmail.com>
2026-03-25 16:37:59 +0700

    add Create Signal Func

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func createTradeSignal(c *gin.Context) {
 	}
 
 	_, err := db.Exec(context.Background(),
-		`INSERT INTO "tbTradeSignal" (symbol, type, price, tp, sl, isActive, status, created_at)
+		`INSERT INTO "tbTradeSignal" (symbol, type, price, tp, sl, isactive, status, created_at)
 		 VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`,
 		t.Symbol, t.Type, t.Price, t.Tp, t.Sl, t.IsActive, t.Status, t.CreatedAt,
 	)
@@ -132,8 +132,8 @@ func getTradeSignals(c *gin.Context) {
 			&t.Price,
 			&t.Tp,
 			&t.Sl,
-			&t.IsActive,
 			&t.Status,
+			&t.IsActive,
 			&t.CreatedAt,
 		)
 		if err != nil {
